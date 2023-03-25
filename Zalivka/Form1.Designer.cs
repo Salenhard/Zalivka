9c16215742c93acb9132bef18d2f9c27babe3843
vlad gurbatov <vlad.gurbatov@gmail.com>
2023-03-25 16:34:40 +0900

    first commit

---
+++ Zalivka/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Zalivka
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonPouring = new System.Windows.Forms.Button();
+            this.buttonFether = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(116, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(672, 426);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // buttonPouring
+            // 
+            this.buttonPouring.Location = new System.Drawing.Point(12, 12);
+            this.buttonPouring.Name = "buttonPouring";
+            this.buttonPouring.Size = new System.Drawing.Size(97, 23);
+            this.buttonPouring.TabIndex = 1;
+            this.buttonPouring.Text = "Заливка";
+            this.buttonPouring.UseVisualStyleBackColor = true;
+            this.buttonPouring.Click += new System.EventHandler(this.buttonPouring_Click);
+            // 
+            // buttonFether
+            // 
+            this.buttonFether.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.buttonFether.Location = new System.Drawing.Point(12, 41);
+            this.buttonFether.Name = "buttonFether";
+            this.buttonFether.Size = new System.Drawing.Size(97, 23);
+            this.buttonFether.TabIndex = 3;
+            this.buttonFether.Text = "Перо";
+            this.buttonFether.UseVisualStyleBackColor = true;
+            this.buttonFether.Click += new System.EventHandler(this.buttonFether_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.buttonClear.Location = new System.Drawing.Point(13, 70);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(97, 23);
+            this.buttonClear.TabIndex = 4;
+            this.buttonClear.Text = "Очистить";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonFether);
+            this.Controls.Add(this.buttonPouring);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonPouring;
+        private System.Windows.Forms.Button buttonFether;
+        private System.Windows.Forms.Button buttonClear;
+    }
+}
+
---
+++ Zalivka/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows;
+
+namespace Zalivka
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+        }
+
+        Bitmap bmp;
+        Painting paintingType = Painting.Feather;
+        struct Pixel
+        {
+            public int x;
+            public int y;
+
+            public Pixel(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+        enum Painting{
+            Feather, Pouring
+        }
+
+        private void buttonPouring_Click(object sender, EventArgs e)
+        {
+            if (paintingType != Painting.Pouring)
+            {
+                paintingType = Painting.Pouring;
+            }
+        }
+
+        private void buttonFether_Click(object sender, EventArgs e)
+        {
+            if (paintingType != Painting.Feather)
+            {
+                paintingType = Painting.Feather;
+            }
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(pictureBox1.Height, pictureBox1.Width);
+            pictureBox1.Image = bmp;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                bmp.SetPixel(e.X, e.Y, Color.Black);
+                pictureBox1.Image = bmp;
+            }
+        }
+
+        private Bitmap pouring(Bitmap bmp, int x, int y, Color color)
+        {
+
+            Stack<Pixel> pixelStack = new Stack<Pixel>();
+            pixelStack.Push(new Pixel(x, y));
+            Color color1 = bmp.GetPixel(x, y);
+            while (pixelStack.Count != 0)
+            {
+                if (bmp.GetPixel(x + 1, y + 1) != color1)
+                    pixelStack.Push(new Pixel(x + 1, y + 1));
+            }
+            return bmp;
+        }
+
+    }
+}
